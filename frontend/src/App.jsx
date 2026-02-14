858a7628b7e563d39dc6f709cc01c5701382d067
marin20231113 <newmarinboy@naver.com>
2026-02-14 11:20:04 +0900

    ch2-1.Hero페이지

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,59 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/footer";
 
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import MainPage from "./Pages/MainPage/MainPage";
+import About from "./Pages/About/About";
+import Board from "./Pages/Board/Board";
+import Contact from "./Pages/Contact/Contact";
+import Leadership from "./Pages/Leadership/Leadership";
+import Services from "./Pages/Services/Services";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
